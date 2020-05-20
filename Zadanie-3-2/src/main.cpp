a29008a8edecd758b8e964be74571c88435af5b8
korneliusz Dudus <korneliusz.dudus@student.wat.edu.pl>
2020-05-20 09:23:58 +0200

    Zadanie 3.2.1-wysyłanie tekstu i liczby przez UART

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
-
+uint8_t i = 0;
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(9600);
+  Serial.println("Witaj programisto!");
+
   }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  
+  Serial.println(i);
+  delay(2000);
+
 }
 
   
